50bbfe86cdd859d42a0622d0c8f942c9280d5997
Lokesh-Pathak <lokesh.pathak915@gmail.com>
2019-04-15 19:48:02 +0530

    load data with swapi at runtime and code modification

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-    url(r'^$', views.HomeView),
-    url(r'^film/$', views.Film.as_view(), name='film'),
-    url(r'^planet/$', views.Planet.as_view(), name='planet'),
+    url(r'^$', views.HomeView.as_view()),
+    url(r'^search/$', views.Search.as_view(), name='search'),
     url(r'^add/$', views.AddData.as_view(), name='add'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,42 +1,57 @@
-import json
-import re
+import requests
 
 from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.views import View
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+
 from app.models import StarWars
-# Create your views here.
 
 
-def HomeView(request):
-    res = StarWars.objects.all()
-    return render(request, "app/index.html", {'data': res})
+class HomeView(View):
+    """
+        :return all star_wars table data which is saved by user
+        :render template
+    """
+    def get(self, request):
+        res = StarWars.objects.all()
+        return render(request, "app/index.html", {'data': res})
 
 
 class AddData(View):
-
+    """
+        :params request data
+        :redirct to root directory after save titles into database
+    """
     def get(self, request):
         StarWars.objects.update_or_create(title=request.GET['name'])
         return HttpResponseRedirect('/')
 
 
-class Film(View):
+def get_data(name, search_by):
+    """
+        :param name, search_by
+        :read swapi url with page number and search element
+        :return: total result
+    """
+    total_results = []
+    for page_num in range(1, 7):
+        response = requests.get('https://swapi.co/api/' + search_by, params={'search': name, 'page': str(page_num)})
+        if response.status_code == 200:
+            data = response.json()
+            total_results = total_results + data['results']
+    return total_results
+
+
+class Search(View):
+    def get(self, request):
+        return HttpResponseRedirect('/')
 
     def post(self, request):
-        with open('films.json') as json_file:
-            data = json.load(json_file)
-            name = re.compile(request.POST['name'], re.IGNORECASE)
-            res = list(filter(lambda x, y=name: y.search(x), data['films']))
-            return render(request, "app/result.html", {'data':res})
-
-
-class Planet(View):
-
-    def post(self, request):
-        with open('planets.json') as json_file:
-            data = json.load(json_file)
-            name = re.compile(request.POST['name'], re.IGNORECASE) 
-            res = list(filter(lambda x, y=name: y.search(x), data['planets']))     
-            return render(request, "app/result.html", {'data':res})
+        """
+            :call get_data method and pass the arguments
+            :return result
+            :render template
+        """
+        res = get_data(request.POST['name'], request.POST['search_by'])
+        return render(request, "app/result.html", {'data': res})
 
